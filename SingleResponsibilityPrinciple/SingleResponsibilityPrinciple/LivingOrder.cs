d91e06bb496832dda7c94c6a13f87084306c1c63
Tom Peeters <tom_ptrs@yahoo.com>
2020-09-08 18:20:02 +0200

    ocp + simpel factory

---
+++ SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/LivingOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleResponsibilityPrinciple
+{
+    class LivingOrder
+    {
+        private OrderProcessor orderProcessor;
+        private Logger logger;
+
+        public LivingOrder(OrderProcessor orderProcessor, Logger logger)
+        {
+            this.orderProcessor = orderProcessor;
+            this.logger = logger;   
+        }
+
+        public void Process(Order order)
+        {
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/OrderFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class OrderFactory
+    {
+        public OrderHandler Create(Order order, OrderProcessor processor)
+        {
+
+            switch (order.OrderType)
+            {
+                case Type.electronics:
+                   return new ElectronicsOrder(processor, processor.logger);
+                   
+                    break;
+                case Type.garden:
+                   return new GarderOrder(processor, processor.logger);
+                 
+                    break;
+                case Type.living:
+                   return new LivingOrder(processor, processor.logger);
+                 
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+    }
+}
